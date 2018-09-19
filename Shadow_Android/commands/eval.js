85ea006bba56e74a88ed241c75e4a3792547b1c4
Ме́филес <40555039+Mephiles-the-Dark@users.noreply.github.com>
2018-09-19 11:19:27 -0500

    Create eval.js

---
+++ Shadow_Android/commands/eval.js
@@ -0,0 +1,22 @@
+/* As a warning eval is dangerous so keep owner restricted to oneself.
+It is used to test features or grab information. */
+const config = require('../config.json')
+const owner = config.owner
+module.exports = async (bot, msg, args) => {
+  if (msg.author.id !== owner) return
+  try {
+    const code = args.join(' ')
+    let evaled = eval(code)
+    if (typeof evaled !== 'string')
+      evaled = require('util').inspect(evaled)
+    msg.channel.send(clean(evaled), { code: 'x1' })
+  } catch (err) {
+    msg.channel.send(`\`ERROR\` \`\`\`x1\n$[clean(err)}\n\`\`\``)
+  }
+}
+function clean(text) {
+  if (typeof (text) === 'string')
+    return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203))
+  else
+    return text
+}
