95206d6090c1c9c91a2732e15c9f1e5484a65a3d
Ме́филес <40555039+Mephiles-the-Dark@users.noreply.github.com>
2018-09-22 23:56:03 -0500

    Update ping.js

--- Shadow_Android/commands/ping.js
+++ Shadow_Android/commands/ping.js
@@ -1,4 +1,4 @@
-module.exports = (client, msg, args) => {
+module.exports = async (client, msg, args) => {
   const m = await msg.channel.send('Чёрт !')
   m.edit(`Latency is: ${m.createdTimestamp - msg.createdTimestamp} ms.  API Latency is: ${Math.round(bot.ping)} ms`)
 }
