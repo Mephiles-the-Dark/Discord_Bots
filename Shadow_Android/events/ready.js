17638ae72dc02f6b70e9a7b57f2ec40645a2dc5c
Ме́филес <40555039+Mephiles-the-Dark@users.noreply.github.com>
2018-09-22 23:19:41 -0500

    Update ready.js

--- Shadow_Android/events/ready.js
+++ Shadow_Android/events/ready.js
@@ -1,11 +1,22 @@
-module.exports = (bot) => {
-  console.log(`Logged in as ${bot.user.tag} (${bot.user.id}).`)
-  console.log(`With ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guild(s).`)
-  android.user.setPresence({
+module.exports = (client) => {
+  console.log(`Logged in as ${client.user.tag} (${client.user.id}).`)
+  console.log(`With ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guild(s).`)
+  // resetting below
+  client.user.setPresence({
     status: 'online',
     game: {
       name: 'Ёж Шэ́доу (2005)',
       type: 0
     }
-  })
+  }).catch(console.error)
+  if (client.user.username !== '<Bot_Username>') {
+    client.user.setUsername('<Bot_Username>')
+      .then(user => console.log('Old username set !'))
+      .catch(console.error)
+  }
+  if (client.user.avatarURL !== '<Standard avatar given by client.user.avatarURL>') {
+    client.user.setAvatar('<Link to standard avatar>')
+      .then(user => console.log(`Old avatar set !`))
+      .catch(console.error)
+  }
 }
