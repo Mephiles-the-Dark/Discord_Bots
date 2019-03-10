eb52cc66795c349066b55fa918fc244a67d45de0
Ме́филес <40555039+Mephiles-the-Dark@users.noreply.github.com>
2019-03-10 11:55:40 -0500

    Create purge.js

---
+++ Shadow_Android/commands/purge.js
@@ -0,0 +1,22 @@
+module.exports = async (client, msg, args) => {
+	if(msg.member.hasPermission('MANAGE_MESSAGES') || msg.mentions.users.first().id === client.id || msg.mentions.users.first().id === author.id) {
+		const user = msg.mentions.users.first()
+  	const amount = !!parseInt(msg.content.split(' ')[1]) ? parseInt(msg.content.split(' ')[1]) : parseInt(msg.content.split(' ')[2])
+  	if (!amount || (amount < 0)) return msg.reply('Must specify an amount to delete!')
+  	if (!amount && !user) return msg.reply('Must specify a user and amount, or just an amount, of messages to purge!')
+    msg.delete().then(
+			msg.channel.fetchMessages({
+  			limit: amount,
+  		}).then((messages) => {
+  			if (user) {
+  				const filterBy = user ? user.id : Client.user.id;
+  				messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount)
+  			}
+      	msg.channel.bulkDelete(messages)
+        	.catch(error => msg.reply(`Could not delete messages because of: ${error}.`))
+  		})
+		)
+  } else {
+		msg.reply('You do not have the MANAGE_MESSAGES permission.');
+}
+}
