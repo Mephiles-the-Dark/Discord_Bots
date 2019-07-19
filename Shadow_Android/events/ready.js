5a3e2019498aa809b31e5e7af92060b4e2169bd0
Ме́филес <40555039+Mephiles-the-Dark@users.noreply.github.com>
2019-07-18 20:55:32 -0500

    presence update to log and load

--- Shadow_Android/events/ready.js
+++ Shadow_Android/events/ready.js
@@ -1,22 +1,14 @@
+const presence = require("../presence.json")
 module.exports = (client) => {
   console.log(`Logged in as ${client.user.tag} (${client.user.id}).`)
   console.log(`With ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guild(s).`)
   // resetting below
   client.user.setPresence({
-    status: 'online',
-    game: {
-      name: 'Ёж Шэ́доу (2005)',
-      type: 0
+      status: presence.status,
+      game: {
+        name: presence.name,
+        type: presence.type,
+        url: presence.url
     }
   }).catch(console.error)
-  if (client.user.username !== '<Bot_Username>') {
-    client.user.setUsername('<Bot_Username>')
-      .then(user => console.log('Old username set !'))
-      .catch(console.error)
-  }
-  if (client.user.avatarURL !== '<Standard avatar given by client.user.avatarURL>') {
-    client.user.setAvatar('<Link to standard avatar>')
-      .then(user => console.log(`Old avatar set !`))
-      .catch(console.error)
-  }
 }
